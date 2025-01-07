569eecb9ebaca7d87e7248b7beec4885ced55481
Maxime DUBOIS <maxime-m.dubois@edf.fr>
2025-01-07 17:23:39 +0100

    fix: thread

--- service/JamService.ts
+++ service/JamService.ts
@@ -2,8 +2,16 @@ import {JamDTO, JamStatus} from "@/DTO/JamDTO";
 import {UserDTO} from "@/DTO/UserDTO";
 
 const user1: UserDTO = {
+    country: "", email: "", imgUrl: "", provider: "", roles: [], userProviderId: "",
     badges: [], events: [], id: 0, jams: [], name: "Titi", playlists: []
 }
+
+
+const user2: UserDTO = {
+    country: "", email: "", imgUrl: "", provider: "", roles: [], userProviderId: "",
+    badges: [], events: [], id: 0, jams: [], name: "Toto", playlists: []
+}
+
 const jam1: JamDTO = {
     background: require('@/assets/images/music-exemple.png'),
     comments: [],
@@ -13,15 +21,27 @@ const jam1: JamDTO = {
     likes: 0,
     name: "jam1",
     participants: [user1],
-    status: JamStatus.UPCOMING,
+    status: JamStatus.RUNNING,
     themes: []
 };
 
+const jam2: JamDTO = {
+    background: require('@/assets/images/music-exemple.png'),
+    comments: [],
+    description: "Bla bla",
+    host: user2,
+    id: 0,
+    likes: 0,
+    name: "jam2",
+    participants: [user2],
+    status: JamStatus.PAUSED,
+    themes: []
+};
 
 const JamService = {
     getJams: () : JamDTO[] => {
         return [
-            jam1, jam1,jam1, jam1
+            jam1, jam2,jam1, jam1
         ];
     },
 };
