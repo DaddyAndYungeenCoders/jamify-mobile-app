050c3b5506ec6a44dc19593afe38f0448e74ebfb
Maxime DUBOIS <maxime-m.dubois@edf.fr>
2025-01-23 00:27:59 +0100

    notif new message

--- hooks/useWebSocketConnection.ts
+++ hooks/useWebSocketConnection.ts
@@ -6,6 +6,9 @@ import { ChatMessage } from "@/types/message.types";
 import { useUserStore } from "@/store/user.store";
 import {NotificationTypes} from "@/types/notification.types";
 import {useNotificationStore} from "@/store/notification.store";
+import firebase from "firebase/compat";
+import User = firebase.User;
+import {userService} from "@/services/user.service";
 
 export const useWebSocketConnection = () => {
   const addMessage = useConversationStore((state) => state.addMessage);
@@ -44,6 +47,11 @@ export const useWebSocketConnection = () => {
       console.log("New message:", data);
       addMessage(data.roomId, data);
       handleConversations();
+      // user: User = userService.getByProviderId(data.senderId)
+      const notif : NotificationTypes = {
+        content: data.content, destId: "", id: "", metadata: {}, roomId: "", timestamp: "", title: "Nouveau Message"
+      }
+      addNotification(notif)
     });
 
     socket.on("new-notification", (data: NotificationTypes) => {
